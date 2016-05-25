2aaffdcf225b1aeaf5a3e10fa2d7d43d62eec785
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2016-05-25 18:46:46 -0300

    support >=RN25

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 // Bridge to:
 // Android: buildConfigField vars set in build.gradle, and exported via ReactConfig
 // iOS: config vars set in xcconfig and exposed via ReactNativeConfig.m
-var React = require('react-native');
-module.exports = React.NativeModules.ReactNativeConfig;
+import { NativeModules } from 'react-native';
+
+export default NativeModules.ReactNativeConfig;
