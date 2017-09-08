b865abd6dd604a76b0a2a1b5530922c3b8fad933
Nick Warner <nickwarner@gmail.com>
2017-09-08 15:26:19 -0400

    Change import priority of React/RCTBridgeModule.h

--- ios/ReactNativeConfig/ReactNativeConfig.h
+++ ios/ReactNativeConfig/ReactNativeConfig.h
@@ -1,7 +1,7 @@
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
 #endif
 
 @interface ReactNativeConfig : NSObject <RCTBridgeModule>
