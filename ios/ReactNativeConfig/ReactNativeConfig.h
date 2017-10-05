5e3aa9188444aa380ce41333c76323f996938980
Martin <SuperDuperMartin@users.noreply.github.com>
2017-10-06 00:00:38 +0200

    fixed breaking change for RN 0.48 (#164)

--- ios/ReactNativeConfig/ReactNativeConfig.h
+++ ios/ReactNativeConfig/ReactNativeConfig.h
@@ -1,9 +1,10 @@
 #if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#elif __has_include("React/RCTBridgeModule.h")
+#import "React/RCTBridgeModule.h"
 #else
 #import "RCTBridgeModule.h"
 #endif
-
 @interface ReactNativeConfig : NSObject <RCTBridgeModule>
 
 + (NSDictionary *)env;
