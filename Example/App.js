85443ac7838af9195fab77e9d96b32615ec72b5f
Frieder Bluemle <frieder.bluemle@gmail.com>
2019-08-29 15:35:01 -0700

    Update react-native to 0.60.5

--- Example/App.js
+++ Example/App.js
@@ -1,25 +1,21 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ *
+ * @format
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 import Config from 'react-native-config';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          API_URL={Config.API_URL}
-        </Text>
+        <Text style={styles.text}>API_URL={Config.API_URL}</Text>
       </View>
     );
   }
