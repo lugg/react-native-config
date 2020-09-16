8616212e61c22e3aa0b8d76714a1f5132757b0be
Bartosz Sosnowski <bzoz@users.noreply.github.com>
2020-09-16 16:28:13 +0200

    Add Windows support (#492)
    Add support for react-native-windows. Update the docs and example and
    add an automatic CI test.

---
+++ Example/windows/ConfigExample/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::ConfigExample::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::ConfigExample::implementation
+
+
---
+++ windows/RNCConfig/ReactPackageProvider.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "ReactPackageProvider.h"
+#include "ReactPackageProvider.g.cpp"
+
+#include <ModuleRegistration.h>
+
+#include "RNCConfig.h"
+
+namespace winrt::RNCConfig::implementation
+{
+  void ReactPackageProvider::CreatePackage(IReactPackageBuilder const& packageBuilder) noexcept
+  {
+    AddAttributedModules(packageBuilder);
+  }
+}
