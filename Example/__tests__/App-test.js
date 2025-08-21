39936663466ff9425683c0438e2074634b5eba03
Protik Biswas <protikbiswas100@microsoft.com>
2025-08-21 20:10:06 +0530

    updating test and removing deprecated selenium driver

--- Example/__tests__/App-test.js
+++ Example/__tests__/App-test.js
@@ -7,8 +7,9 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { createLegacyRoot } from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const root = createLegacyRoot(<App />);
+  root.unmount();
 });
