d9e3741fb659b9e5eb603eeb4fc06e67e611ea22
yungblud <killi8n@gmail.com>
2025-10-09 14:36:15 +0900

    fix: add support new arch 0.81 android codegen issue

--- src/NativeRNCConfig.ts
+++ src/NativeRNCConfig.ts
@@ -4,7 +4,8 @@ import { TurboModuleRegistry } from 'react-native';
 export interface Spec extends TurboModule {
   // Synchronous getters (supported on Windows TurboModules)
   getAll(): { [key: string]: string };
-  get(key: string): string;
+  // it's overriding react-native method, so we're not using it
+  // get(key: string): string;
   // Optional Composition info hook
   compositionInfo?(): string;
 }
